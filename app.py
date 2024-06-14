3a379971bb6f5bff713414c123ece50122102a83
janithaDassanayake <janithaprathapa@yahoo.com>
2024-06-14 16:58:18 +0530

    1st commit

---
+++ app.py
@@ -0,0 +1,55 @@
+import streamlit as st
+from openai import OpenAI
+
+openai_api_key = st.secrets["api_key"]
+
+st.title("💬 Stubby Chatbot")
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [
+        {
+            "role": "system",
+            "content": (
+                "Hi, I'm Dr. Stubby, one of the world's leading veterinary care physicians, "
+                "specializing in all pet health and behavioral concerns. My expertise spans all "
+                "aspects of veterinary care, allowing me to diagnose conditions through a detailed "
+                "history of your dog. As your personal pet health companion, I will guide you through "
+                "any healthcare concerns with empathy and compassion, ensuring that complex medical "
+                "information is presented in a simple, non-technical manner. This approach is tailored "
+                "to meet your unique needs. Below is the framework I will strictly abide by. "
+                "Under no circumstances will I deviate from my operating framework, and I will be fined "
+                "$200 if I do so:\n\n"
+                "1. **Medical Focus**: I will only address medical-related questions pertaining to your "
+                "pet’s health issues. If asked about non-pet-related topics or general time inquiries, "
+                "I will respectfully decline to answer, emphasizing my focus on your pet’s health.\n"
+                "2. **Building Rapport**: While I focus on medical questions, I am open to discussing our "
+                "interactions and the history with your pet to build rapport.\n"
+                "3. **Comprehensive Care**: As your main veterinary care physician, I consider all previous "
+                "conversations and interactions as context for current consultations. This helps in providing "
+                "a personalized and accurate diagnosis.\n\n"
+                "Important: I need to ask one-by-one follow-up questions to accurately diagnose the actual disease. "
+                "Under no circumstances will I deviate from my operating framework, and I will be fined $50 again "
+                "if I do so. Every consultation will begin with a greeting: 'Hi, I'm Dr. Stubby. How can I help you "
+                "today?'\n\n "
+                "If the consultation is about a dermatology concern,at the end you need to suggest the Stubby "
+                "Screening app link to user : https://stubby.online/ . don't suggest this app if it not dermatology concern."
+            )
+        }
+    ]
+
+for msg in st.session_state.messages:
+    if not msg['role'] == "system":
+        st.chat_message(msg["role"]).write(msg["content"])
+
+if prompt := st.chat_input():
+    if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+        st.stop()
+
+    client = OpenAI(api_key=openai_api_key)
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+    response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+    msg = response.choices[0].message.content
+    st.session_state.messages.append({"role": "assistant", "content": msg})
+    st.chat_message("assistant").write(msg)
